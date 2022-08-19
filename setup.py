fb20e11cac40a32093d730c4e2c153c985631100
Ralph Thesen <mail@redimp.de>
2022-08-19 23:42:55 +0200

    mistune back to 0.8.4 till otterwiki renderer is fixed

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "Jinja2>=2.9",
         "gitpython",
         "cython",
-        "mistune==2.0.3",
+        "mistune==0.8.4",
         "pygments",
         "Pillow",
         "unidiff",
