98ec1d8646f5b28b1b374b4f5d43b4b6cd8747f6
Ralph Thesen <mail@redimp.de>
2022-10-02 12:28:42 +0200

    New Dockerfile image with multistage build
    Reworked the entire build process, not using tiangolo/uwsgi-nginx
    as base image anymore, wil use nginx instead.
    Due to this changes supervisord and uwsgi need configuration via
    files from the docker/ directory. nginx will be configured via
    the entrypoint.sh to make use of environment variables.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -57,19 +57,10 @@ for EV in MAIL_PORT MAIL_USE_TLS MAIL_USE_SSL; do
     fi
 done
 
-# configure uwsgi
-if [ ! -f ${UWSGI_INI} ]; then
-    cp /app/otterwiki/uwsgi.ini ${UWSGI_INI}
-    echo -e "uid = www-data\ngid = www-data" >> ${UWSGI_INI}
-fi
-
 chown -R www-data:www-data /app-data
 
 # Get the maximum upload file size for Nginx, default to 0: unlimited
 USE_NGINX_MAX_UPLOAD=${NGINX_MAX_UPLOAD:-0}
-# Generate Nginx config for maximum upload file size
-echo "client_max_body_size $USE_NGINX_MAX_UPLOAD;" > /etc/nginx/conf.d/upload.conf
-
 # Get the number of workers for Nginx, default to 1
 USE_NGINX_WORKER_PROCESSES=${NGINX_WORKER_PROCESSES:-1}
 # Modify the number of worker processes in Nginx config
@@ -78,13 +69,14 @@ sed -i "/worker_processes\s/c\worker_processes ${USE_NGINX_WORKER_PROCESSES};" /
 # Get the URL for static files from the environment variable
 USE_STATIC_URL=${STATIC_URL:-'/static'}
 # Get the absolute path of the static files from the environment variable
-USE_STATIC_PATH=${STATIC_PATH:-'/app/static'}
+USE_STATIC_PATH=${STATIC_PATH:-'/app/otterwiki/static'}
 # Get the listen port for Nginx, default to 80
 USE_LISTEN_PORT=${LISTEN_PORT:-80}
 
 # Generate Nginx config first part using the environment variables
 echo "server {
     listen ${USE_LISTEN_PORT};
+    client_max_body_size $USE_NGINX_MAX_UPLOAD;
     location / {
         try_files \$uri @app;
     }
@@ -94,44 +86,14 @@ echo "server {
     }
     location $USE_STATIC_URL {
         alias $USE_STATIC_PATH;
-    }" > /etc/nginx/conf.d/nginx.conf
+    }" > /etc/nginx/conf.d/default.conf
 
-# If STATIC_INDEX is 1, serve / with /static/index.html directly (or the static URL configured)
-if [[ $STATIC_INDEX == 1 ]] ; then 
-echo "    location = / {
-        index $USE_STATIC_URL/index.html;
-    }" >> /etc/nginx/conf.d/nginx.conf
-fi
+# # FIXME
+# echo "    set_real_ip_from 172.21.0.1;
+#     real_ip_header X-Real-IP;
+#     real_ip_recursive on;" >> /etc/nginx/conf.d/nginx.conf
 
-echo "    set_real_ip_from 172.21.0.1;
-    real_ip_header X-Real-IP;
-    real_ip_recursive on;" >> /etc/nginx/conf.d/nginx.conf
-
-# Finish the Nginx config file
-echo "}" >> /etc/nginx/conf.d/nginx.conf
-
-# wsgi.file_wrapper is an optimization of the WSGI standard. In some
-# corner case it can raise an error. For example when returning an
-# in-memory bytes buffer (io.Bytesio) in Python 3.5.
-# see https://uwsgi-docs.readthedocs.io/en/latest/ThingsToKnow.html
-if ! grep -q "^command=/usr/local/bin/uwsgi.*--wsgi-disable-file-wrapper" /etc/supervisor/conf.d/supervisord.conf; then
-    sed -i '/^command=\/usr\/local\/bin\/uwsgi/ s/$/ --wsgi-disable-file-wrapper/' /etc/supervisor/conf.d/supervisord.conf
-fi
-
-# enable threads, which are disabled by default
-# see https://uwsgi-docs.readthedocs.io/en/latest/WSGIquickstart.html#a-note-on-python-threads
-if ! grep -q "^command=/usr/local/bin/uwsgi.*--enable-threads" /etc/supervisor/conf.d/supervisord.conf; then
-       sed -i '/^command=\/usr\/local\/bin\/uwsgi/ s/$/ --enable-threads/' /etc/supervisor/conf.d/supervisord.conf
-fi
-
-# tell nginx not to start an instance on its own
-if ! grep -q "^command=/usr/sbin/nginx.*-g 'daemon off;'" /etc/supervisor/conf.d/supervisord.conf; then
-    sed -i "/^command=\/usr\/sbin\/nginx/ s/$/ -g 'daemon off;'/" /etc/supervisor/conf.d/supervisord.conf
-fi
-
-# and take care that this is not duplicate in the /etc/nginx/nginx.conf
-if grep -q "^daemon off;" /etc/nginx/nginx.conf; then
-    sed -i "s/^daemon off;/# daemon off;/" /etc/nginx/nginx.conf
-fi
+# close the server block
+echo "}" >> /etc/nginx/conf.d/default.conf
 
 exec "$@"
