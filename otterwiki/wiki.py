6bfc79dd4867ef9c9f703005389009995816e199
marcrleonard <marc.r.leonard@gmail.com>
2022-10-03 06:08:13 -0600

    add files into the edit view

--- otterwiki/wiki.py
+++ otterwiki/wiki.py
@@ -285,6 +285,10 @@ class Page:
             except StorageNotFound:
                 content = ""
             content = content.rstrip()
+
+        # get file listing
+        files = [f.data for f in self._attachments() if f.metadata is not None]
+
         return render_template(
             "editor.html",
             pagename=self.pagename,
@@ -292,6 +296,7 @@ class Page:
             content_editor=content,
             cursor_line=cursor_line,
             cursor_ch=cursor_ch,
+            files = files
         )
 
     def save(self, content, commit, author):
