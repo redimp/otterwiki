586d5cddd8e08fd4e16aa7afd0e8362741436b2a
Ralph Thesen <mail@redimp.de>
2022-10-16 23:55:06 +0200

    enable to delete multiple files with delete() and delete recursive

--- otterwiki/gitstorage.py
+++ otterwiki/gitstorage.py
@@ -248,7 +248,9 @@ class GitStorage(object):
         return self.repo.git.diff(rev_a, rev_b, filename)
 
     def delete(self, filename, message=None, author=None):
-        self.repo.index.remove([filename], working_tree=True)
+        if not type(filename) == list:
+            filename=[filename]
+        self.repo.index.remove(filename, working_tree=True, r=True)
         actor = git.Actor(author[0], author[1])
         if message is None:
             message = "Deleted {}.".format(filename)
