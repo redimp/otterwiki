75883785d8e80aa2a2c2789ab0770561a2a040c9
Ralph Thesen <mail@redimp.de>
2022-10-29 18:38:51 +0200

    generalized js function and moved it into .js file

--- otterwiki/static/js/otterwiki.js
+++ otterwiki/static/js/otterwiki.js
@@ -1,6 +1,19 @@
-/* vim: set et sts=4 ts=4 ai: */
+/* vim: set et sts=4 ts=4 sw=4 ai: */
 
 var otterwiki = {
+    // Toggle display block/none
+    toggleClass: function(show, classname) {
+        const boxes = document.getElementsByClassName(classname);
+        if (show) {
+            for (const box of boxes) {
+                box.style.display = 'block';
+            }
+        } else {
+            for (const box of boxes) {
+                box.style.display = 'none';
+            }
+        }
+    },
     // Toggle modal (using Javascript)
     toggleModal: function(modalId) {
         var modal = document.getElementById(modalId);
