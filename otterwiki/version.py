c7995dba8fe9f76507e0c16b2b510efe2c6aefa3
Ralph Thesen <mail@redimp.de>
2023-10-16 22:10:07 +0200

    reworked tbump __version__ string

--- otterwiki/version.py
+++ otterwiki/version.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
-__version__ = "2.0.0-beta31"
+# version_onfo managed by tbump
+version_info = (2, 0, 0, "beta31")
+
+# build version string from version_info
+__version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}" + \
+        f"-{version_info[3]}" if len(version_info[3])>0 else ""
 
 # vim: set et ts=8 sts=4 sw=4 ai:
