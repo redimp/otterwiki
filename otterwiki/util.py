db8e6880056a7855dfe86aa770868bfd99676ffd
Ralph Thesen <mail@redimp.de>
2023-10-27 00:14:08 +0200

    util: added ttl_lru_cache

--- otterwiki/util.py
+++ otterwiki/util.py
@@ -8,6 +8,23 @@ from email.utils import parseaddr
 import random
 import string
 import mimetypes
+import time
+from functools import lru_cache
+
+
+def ttl_lru_cache(ttl: int = 60, maxsize: int = 128):
+    """
+    Time aware lru caching thx to https://stackoverflow.com/a/73026174/212768
+    """
+    def wrapper(func):
+
+        @lru_cache(maxsize)
+        def inner(__ttl, *args, **kwargs):
+            # Note that __ttl is not passed down to func,
+            # as it's only used to trigger cache miss after some time
+            return func(*args, **kwargs)
+        return lambda *args, **kwargs: inner(time.time() // ttl, *args, **kwargs)
+    return wrapper
 
 
 def sizeof_fmt(num, suffix="B"):
