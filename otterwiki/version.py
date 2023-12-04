c0908b268824167d63aeb7eea5ad547675fff9ff
Ralph Thesen <mail@redimp.de>
2023-12-04 23:19:35 +0100

    fixed the version string

--- otterwiki/version.py
+++ otterwiki/version.py
@@ -5,6 +5,6 @@ version_info = (2, 0, 0, "")
 
 # build version string from version_info
 __version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}" + \
-        f"-{version_info[3]}" if len(version_info[3])>0 else ""
+        (f"-{version_info[3]}" if len(version_info[3])>0 else "")
 
 # vim: set et ts=8 sts=4 sw=4 ai:
