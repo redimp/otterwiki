823929c8a2c9f83e0fa5ec526d9846061a2b88a4
Ralph Thesen <mail@redimp.de>
2025-12-13 11:58:49 +0100

    fixed merge fail

--- otterwiki/wiki.py
+++ otterwiki/wiki.py
@@ -543,7 +543,6 @@ class Page:
             canonical_url=canonical_url,
             library_requirements=library_requirements,
             canonical_url=canonical_url,
-            library_requirements=library_requirements,
         )
 
     def preview(self, content=None, cursor_line=None):
