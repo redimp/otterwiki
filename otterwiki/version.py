2c1a4a4a2445f71995fa407f0bf97c8cdcbf4749
Ralph Thesen <mail@redimp.de>
2026-01-18 12:51:01 +0100

    Bump to 2.16.0

--- otterwiki/version.py
+++ otterwiki/version.py
@@ -2,7 +2,7 @@
 # vim: set et ts=8 sts=4 sw=4 ai:
 
 # version_onfo managed by tbump
-version_info = (2, 15, 2, "")
+version_info = (2, 16, 0, "")
 
 # build version string from version_info
 __version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}" + (
